162607885c36d393910408e6f242db58bf570729
ourfalia <ourfalia@tcd.ie>
2023-03-27 05:03:45 +0000

    create reservation page in templates and add item table to database

--- arabica/urls.py
+++ arabica/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from arabicafood.views import get_home
+from arabicafood.views import get_home, make_reservation
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', make_reservation, name='reservation'),
     path('', get_home, name='get_home'),
 ]
--- arabicafood/views.py
+++ arabicafood/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def get_home(request):
     return render(request, 'arabicafood/home.html')
 
+def make_reservation(request):
+    return render(request, 'arabicafood/reservation.html')
