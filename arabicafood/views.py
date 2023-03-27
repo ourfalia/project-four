bb55b6c9eb37ecfa627ae39df8050646681fd72e
ourfalia <ourfalia@tcd.ie>
2023-03-27 14:17:19 +0000

    render items in reservation.html

--- arabicafood/views.py
+++ arabicafood/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Item
 
 
 def get_home(request):
     return render(request, 'arabicafood/home.html')
 
 def make_reservation(request):
-    return render(request, 'arabicafood/reservation.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'arabicafood/reservation.html', context)
