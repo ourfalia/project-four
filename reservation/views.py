934bca6a8c931e869ea21b8a5b3e8dcd71c56030
ourfalia <ourfalia@tcd.ie>
2023-03-30 12:04:34 +0000

    correct indentation problems

--- reservation/views.py
+++ reservation/views.py
@@ -5,6 +5,7 @@ from reservation.models import Reservation
 
 # Create your views here.
 
+
 def reserve_table(request):
     reserve_form = ReserveTableForm()
 
@@ -14,7 +15,7 @@ def reserve_table(request):
         if reserve_form.is_valid():
             reserve_form.save()
 
-    context = {'form' : reserve_form}
+    context = {'form': reserve_form}
 
     return render(request, 'reservation/reservation.html', context)
 
