d88207276e6d9a33079c383825cc085768757f0a
ourfalia <ourfalia@tcd.ie>
2023-03-30 10:08:58 +0000

    add views and urls to the reservation app

--- arabica/urls.py
+++ arabica/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from arabicafood.views import get_home
+from arabicafood.views import get_home, reserve_table
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', get_home, name='get_home'),
-    path('home', get_home, name='home'),
+    path('home/', get_home, name='home'),
+    path('reservation/', reserve_table, name='reservation'),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from.models import Reservation
+from reservation.models import Reservation
 
 # Create your views here.
+
+def reserve_table(request):
+    return render(request, 'reservation/reservation.html')
